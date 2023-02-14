7b9eca7090272b58afa44cb07423b99afad353c7
christina-nickolova <christinaxnickolova@gmail.com>
2023-02-14 21:13:30 +0200

    Fixed code

--- Programming-Fundamentals-Java/Lists - Exercise/AppendArrays_07.java
+++ Programming-Fundamentals-Java/Lists - Exercise/AppendArrays_07.java
@@ -1,29 +1,18 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AppendArrays_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] arrays = scanner.nextLine().split("\\|");
+        List<String> arrays = Arrays
+                .stream(scanner.nextLine().split("\\|"))
+                .collect(Collectors.toList());
 
-        List<String[]> numbers = new ArrayList<>();
-
-        for (String currentArray : arrays) {
-            String[] array = currentArray.split("\s+");
-            numbers.add(array);
-        }
-        Collections.reverse(numbers);
-        StringBuilder result = new StringBuilder();
-
-        for (String[] array : numbers) {
-
-            for (String number : array) {
-                result.append(number);
-            }
-        }
-
-        for (int i = 0; i < result.length(); i++) {
-            System.out.print(result.charAt(i) + " ");
-        }
+        Collections.reverse(arrays);
+        System.out.println(arrays.toString()
+                .replaceAll("[\\[\\],]", "")
+                .trim()
+                .replaceAll("\\s+", " "));
     }
 }
