ce9275a6847b78e366a9ef22efb71812e9332d00
christina-nickolova <christinaxnickolova@gmail.com>
2023-03-09 21:51:10 +0200

    Added new solution

---
+++ Programming-Fundamentals-Java/Text Processing - Lab/ReverseStrings_01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReverseStrings_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        while (!text.equals("end")) {
+            StringBuilder reversed = new StringBuilder(text);
+            reversed.reverse();
+            System.out.printf("%s = %s%n", text, reversed);
+            text = scanner.nextLine();
+        }
+    }
+}
