9b266091a459229c51a1803eed55ac6a18d5d293
christina-nickolova <christinaxnickolova@gmail.com>
2023-03-16 05:40:39 +0200

    Fixed solution

--- Programming-Fundamentals-Java/Lists - More Exercise/CarRace_02.java
+++ Programming-Fundamentals-Java/Lists - More Exercise/CarRace_02.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -7,37 +8,38 @@ public class CarRace_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> numbers = Arrays
+        List<Double> numbers = Arrays
                 .stream(scanner.nextLine().split(" "))
-                .map(Integer::parseInt)
+                .map(Double::parseDouble)
                 .collect(Collectors.toList());
 
-        int finish = numbers.size() / 2;
-        double firstRacerTime = 0;
+        int finish = numbers.size() / 2 + 1;
 
-        for (int i = 0; i < finish; i++) {
-            int currentTime = numbers.get(i);
-            firstRacerTime += currentTime;
+        List<Double> leftTime = numbers.subList(0, finish - 1);
+        List<Double> rightTime = numbers.subList(finish, numbers.size());
+        Collections.reverse(rightTime);
 
-            if (currentTime == 0) {
-                firstRacerTime = firstRacerTime * 0.8;
-            }
-        }
-        double secondRacerTime = 0;
+        double left = getTime(leftTime);
+        double right = getTime(rightTime);
 
-        for (int i = finish + 1; i < numbers.size(); i++) {
-            int currentTime = numbers.get(i);
-            secondRacerTime += currentTime;
-
-            if (currentTime == 0) {
-                secondRacerTime = secondRacerTime * 0.8;
-            }
-        }
-
-        if (firstRacerTime < secondRacerTime) {
-            System.out.printf("The winner is left with total time: %.1f%n", firstRacerTime);
+        if (left <= right) {
+            System.out.printf("The winner is left with total time: %.1f", left);
         } else {
-            System.out.printf("The winner is right with total time: %.1f%n", secondRacerTime);
+            System.out.printf("The winner is right with total time: %.1f", right);
         }
+    }
+
+    public static double getTime(List<Double> list) {
+        double totalTime = 0;
+
+        for (Double time : list) {
+
+            if (time == 0) {
+                totalTime *= 0.8;
+            } else {
+                totalTime += time;
+            }
+        }
+        return totalTime;
     }
 }
