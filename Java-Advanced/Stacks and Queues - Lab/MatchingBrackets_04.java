dd978debaa8c820dee96b2ca6b62c38a148fb42d
Christina Nickolova <122849030+christina-nickolova@users.noreply.github.com>
2024-09-10 23:24:07 +0300

    Modified solution

--- Java-Advanced/Stacks and Queues - Lab/MatchingBrackets_04.java
+++ Java-Advanced/Stacks and Queues - Lab/MatchingBrackets_04.java
@@ -2,22 +2,23 @@ import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class MatchingBrackets_04 {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+
         String expression = scanner.nextLine();
-        // 1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
 
         ArrayDeque<Integer> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < expression.length(); i++) {
-            char currentSymbol = expression.charAt(i);
+        for (int index = 0; index < expression.length(); index++) {
+            char currentSymbol = expression.charAt(index);
 
             if (currentSymbol == '(') {
-                stack.push(i);
+                stack.push(index);
             } else if (currentSymbol == ')') {
-                int startIndex = stack.pop();
-                String subExpression = expression.substring(startIndex, i + 1);
-                System.out.println(subExpression);
+                String substring = expression.substring(stack.pop(), index + 1);
+                System.out.println(substring);
             }
         }
     }
