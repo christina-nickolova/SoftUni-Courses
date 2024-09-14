45513198a0d813d6a05a7fab79d43d6cdc67915b
Christina Nickolova <122849030+christina-nickolova@users.noreply.github.com>
2024-09-14 10:10:36 +0300

    Update BasicStackOperations_02.java

--- Java-Advanced/Stacks and Queues - Exercise/BasicStackOperations_02.java
+++ Java-Advanced/Stacks and Queues - Exercise/BasicStackOperations_02.java
@@ -1,38 +1,51 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class BasicStackOperations_02 {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        int[] integers = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        int[] input = readIntArray(scanner);
+        int elementsToPush = input[0];
+        int elementsToPop = input[1];
+        int elementToCheck = input[2];
 
-        int n = integers[0];
-        int s = integers[1];
-        int x = integers[2];
+        int[] elements = readIntArray(scanner);
 
         ArrayDeque<Integer> stack = new ArrayDeque<>();
 
-        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s++"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-
-        for (int i = 0; i < n; i++) {
-            int currentNumber = numbers[i];
-            stack.push(currentNumber);
+        for (int i = 0; i < elementsToPush; i++) {
+            int currentElement = elements[i];
+            stack.push(currentElement);
         }
 
-        for (int i = 0; i < s; i++) {
+        for (int i = 0; i < elementsToPop; i++) {
             stack.pop();
         }
 
-        if (stack.size() == 0) {
-            System.out.println("0");
-        } else if (stack.contains(x)) {
+        if (stack.isEmpty()) {
+            System.out.println(0);
+        } else if (stack.contains(elementToCheck)) {
             System.out.println("true");
         } else {
-            System.out.println(Collections.min(stack));
+            int smallestElement = stack.peek();
+
+            for (int element : stack) {
+
+                if (smallestElement > element) {
+                    smallestElement = element;
+                }
+            }
+            System.out.println(smallestElement);
         }
+    }
+
+    private static int[] readIntArray(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 }
