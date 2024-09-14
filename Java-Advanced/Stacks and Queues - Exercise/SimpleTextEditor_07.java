91c1cd85eb0eccc9c2c8777b8baf571a221a5a9a
Christina Nickolova <122849030+christina-nickolova@users.noreply.github.com>
2024-09-14 14:00:04 +0300

    Update SimpleTextEditor_07.java

--- Java-Advanced/Stacks and Queues - Exercise/SimpleTextEditor_07.java
+++ Java-Advanced/Stacks and Queues - Exercise/SimpleTextEditor_07.java
@@ -2,43 +2,45 @@ import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class SimpleTextEditor_07 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int operations = Integer.parseInt(scanner.nextLine());
 
-        StringBuilder text = new StringBuilder();
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int commandsCount = Integer.parseInt(scanner.nextLine());
 
         ArrayDeque<String> stack = new ArrayDeque<>();
+        StringBuilder text = new StringBuilder();
 
-        for (int i = 0; i < operations; i++) {
+        for (int i = 0; i < commandsCount; i++) {
             String[] input = scanner.nextLine().split("\\s+");
-            String number = input[0];
 
-            if (number.equals("1")) {
-                String currentText = input[1];
-                text.append(currentText);
-                stack.push(String.valueOf(text));
-            } else if (number.equals("2")) {
-                int count = Integer.parseInt(input[1]);
+            String command = input[0];
 
-                if (count > text.length()) {
-                    text = new StringBuilder();
-                } else {
+            switch (command) {
+                case "1":
+                    String string = input[1];
+                    text.append(string);
+                    stack.push(String.valueOf(text));
+                    break;
+                case "2":
+                    int count = Integer.parseInt(input[1]);
                     text.delete(text.length() - count, text.length());
                     stack.push(String.valueOf(text));
-                }
-            } else if (number.equals("3")) {
-                int index = Integer.parseInt(input[1]);
-                String element = text.substring(index - 1, index);
-                System.out.println(element);
-            } else if (number.equals("4")) {
-                stack.pop();
+                    break;
+                case "3":
+                    int index = Integer.parseInt(input[1]);
+                    System.out.println(text.charAt(index - 1));
+                    break;
+                case "4":
+                    stack.pop();
 
-                if (stack.isEmpty()) {
-                    text =new StringBuilder();
-                } else {
-                    text = new StringBuilder(stack.peek());
-                }
+                    if (!stack.isEmpty()) {
+                        text = new StringBuilder(stack.peek());
+                    } else {
+                        text = new StringBuilder();
+                    }
+                    break;
             }
         }
     }
